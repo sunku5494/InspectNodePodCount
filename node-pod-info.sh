4b0cef2373d80db531d0f20e4ab8f39ee82d6db9
vsunku <vsunku@redhat.com>
2024-12-19 10:16:16 +0530

    correcting logs and comments

--- node-pod-info.sh
+++ node-pod-info.sh
@@ -25,23 +25,23 @@ cleanup() {
 trap cleanup TERM
 
 while :; do
-	#collect the count of ipv4 files created under CNI_NETWORK_DIR
+	#Counting the list of ipv4 files created in $CNI_NETWORK_DIR
 	CNI_NETWORK_DIR="/var/lib/cni/networks/openshift-sdn"
 	ALLOCATED_IPS=$(find $CNI_NETWORK_DIR -type f -regextype posix-extended -regex '.*/([0-9]{1,3}\.){3}[0-9]{1,3}$' | wc -l)
 
-	#collect the count of pods or containers attached to cluster default network created with in a node
-	#filter out the pods that dont have Ip address assigned
+	#Counting the pods in a worker node that are attached to the cluster's default network, status is running
+	#and have an IP address assigned to them
 	POD_COUNT=$(oc --kubeconfig=/var/lib/kubelet/kubeconfig get pods -A \
 		--field-selector spec.nodeName="$(hostname)",status.podIP!=null,status.phase="Running" \
 		-o json | jq '[.items[] | select(.metadata.annotations["k8s.v1.cni.cncf.io/network-status"] | fromjson? | any(.default))] | length')
 
-	if [ "$POD_COUNT" -lt "$ALLOCATED_IPS" ]; then #validate if count types are string or integer
-		echo "FATAL: $(date '+%Y-%m-%d %H:%M:%S') - POD COUNT is less than the POD IPs Allocated"
+	if [ "$POD_COUNT" -lt "$ALLOCATED_IPS" ]; then
+		echo "ERROR: $(date '+%Y-%m-%d %H:%M:%S') - POD COUNT is less than the POD IPs Allocated. A few stale IPv4 files could be existing in the $(CNI_NETWORK_DIR)"
 	elif [ "$POD_COUNT" -gt "$ALLOCATED_IPS" ]; then
 		echo "ERROR: $(date '+%Y-%m-%d %H:%M:%S') - POD COUNT is greater than the POD IPs Allocated"
 	else
 		echo "INFO: $(date '+%Y-%m-%d %H:%M:%S') - POD and POD IP Count are same"
 	fi
-	echo "POD COUNT: ${POD_COUNT} and PODIPs COUNT: ${ALLOCATED_IPS}"
+	echo "INFO: $(date '+%Y-%m-%d %H:%M:%S') - POD COUNT: ${POD_COUNT} and PODIPs COUNT: ${ALLOCATED_IPS}"
 	sleep 3600
 done
